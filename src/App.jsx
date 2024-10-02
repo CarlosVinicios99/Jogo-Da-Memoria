07881b98ba672dcf6bb8d829928cc1ff68ede7ee
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-10-01 21:55:51 -0300

    feature: criação e organização inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Game from './components/Game'
+
+function App() {
+    return (
+      <>
+      <h1>Jogo da memória</h1>
+        <Game/>
+      </>
+    )
+}
+
+export default App
---
+++ src/components/Board.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+
+const Board = () => {
+  return (
+    <div className="board">
+        <Card/>
+    </div>
+  )
+}
+
+export default Board
