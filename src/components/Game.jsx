62ed37a949c33a9aa31cb26da034a0a6c8999958
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-10-03 19:16:21 -0300

    feature: criação da lógica para gerar as cartas

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import Board from './Board'
 
+
+const shuffleArray = (array) => {
+
+}
+
+const generateCards = () => {
+  const values = ["A", "B", "C", "D", "E", "F", "G", "H"]
+
+  const cards = values.map((value) => ({
+    value,
+    isFlip: false
+  }))
+
+  const duplicatedCards = cards.concat([...cards]).map((card, index) => ({...card, index}))
+
+  console.log(cards)
+}
+
+generateCards()
+
 const Game = () => {
   return (
     <div className="game">
